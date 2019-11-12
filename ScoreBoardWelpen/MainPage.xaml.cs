6e3e08470d556e888373ef096c961beb5ed8b952
PepijnHogervorst <pepijnh443@gmail.com>
2019-11-12 20:23:11 +0100

    #0 Layout pages created and nav view implemented

--- ScoreBoardWelpen/MainPage.xaml.cs
+++ ScoreBoardWelpen/MainPage.xaml.cs
@@ -31,11 +31,35 @@ namespace ScoreBoardWelpen
             this.InitializeComponent();
 
             IO = new Classes.GPIO();
-            //
+
+            //Events
             if (IO.HasGPIO)
             {
                 IO.ArcadeBtnPressed += IO_ArcadeBtnPressed;
             }
+            this.nvMenu.ItemInvoked += NvMenu_ItemInvoked;
+        }
+
+        private void NvMenu_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItem is TextBlock ItemContent)
+            {
+                switch (ItemContent.Tag)
+                {
+                    case "Scoreboard":
+                        contentFrame.Navigate(typeof(Screens.Scoreboard));
+                        break;
+
+                    case "GroupEdit":
+                        contentFrame.Navigate(typeof(Screens.GroupEdit));
+                        break;
+
+                    case "Device":
+                        contentFrame.Navigate(typeof(Screens.Device));
+                        break;
+                }
+            }
+
         }
 
         private async void IO_ArcadeBtnPressed(Windows.Devices.Gpio.GpioPin sender, Windows.Devices.Gpio.GpioPinValueChangedEventArgs args)
@@ -44,9 +68,28 @@ namespace ScoreBoardWelpen
 
             await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
                 //UI code here
-                this.TxtPressed.Text = $"Button with pin {sender.PinNumber} number of times pressed: {counter}";
+                //this.TxtPressed.Text = $"Button with pin {sender.PinNumber} number of times pressed: {counter}";
             });
             
+        }
+
+        private void nvMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in nvMenu.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "Scoreboard")
+                {
+                    nvMenu.SelectedItem = item;
+                    break;
+                }
+            }
+            contentFrame.Navigate(typeof(Screens.Scoreboard));
+        }
+
+        private void nvMenu_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+
         }
     }
 }
