10701ad9dc6a909a1fa9ee99f5567c03710648eb
PepijnHogervorst <pepijnh443@gmail.com>
2019-12-19 21:45:43 +0100

    #5 Written arduino code, need to test led strips

--- Arduino/LEDcontrollerScoreboard/LEDcontrollerScoreboard.ino
+++ Arduino/LEDcontrollerScoreboard/LEDcontrollerScoreboard.ino
@@ -10,14 +10,16 @@
 /************************************************************************/
 /*                             DEFINITIONS                              */
 /************************************************************************/
-#define LED_GROUP_1             5
-#define LED_GROUP_2             5
-#define LED_GROUP_3             5
+#define LED_GROUP_1             2
+#define LED_GROUP_2             3
+#define LED_GROUP_3             4
 #define LED_GROUP_4             5
-#define LED_GROUP_5             5
-#define LED_GROUP_6             5
+#define LED_GROUP_5             6
+#define LED_GROUP_6             7
 
 #define NUM_OF_PIXELS           100
+
+#define BRIGHTNESS              50
 /************************************************************************/
 /*                                                                      */
 /************************************************************************/
@@ -25,16 +27,200 @@
 /************************************************************************/
 /*                            GLOBAL VARIABLES                          */
 /************************************************************************/
-Adafruit_NeoPixel pixels_group1 = Adafruit_NeoPixel(NUM_OF_PIXELS, LED_GROUP_1, NEO_RGB  + NEO_KHZ800);
+// One neopixel object is used, and the pin is switched when a different strip is needed to put on.
+Adafruit_NeoPixel led_strip = Adafruit_NeoPixel(NUM_OF_PIXELS, LED_GROUP_1, NEO_RGB  + NEO_KHZ800);
+
+String inputString = "";
+bool messageReceived = false;
+
+// Lookup table 
+byte neopix_gamma[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
 /************************************************************************/
 /*                                                                      */
 /************************************************************************/
+
+/************************************************************************/
+/*                                 SETUP                                */
+/************************************************************************/
 void setup() {
-  // put your setup code here, to run once:
+  // Init serial communication:
+  Serial.begin(9600);
+  Serial1.begin(9600);
 
+  // Allocate memory for string
+  inputString.reserve(200);
+  
+  // Init the led strip
+  led_strip.setBrightness(BRIGHTNESS);
+  led_strip.begin();
+  led_strip.show();
+
+  // Extra randomness for start color 
+  randomSeed(analogRead(0));
+
+  Serial.println("Initialization done!");
 }
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
 
+/************************************************************************/
+/*                                MAIN LOOP                             */
+/************************************************************************/
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Check if serial message is received
+  if(messageReceived)
+  {
+    // Handle event
+    Serial.println("Message received: " + inputString);
+    WriteLedStrip();
+    
+    // Clear event
+    messageReceived = false;
+    inputString = "";
+  }
 }
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+
+/************************************************************************/
+/*                              EVENTS                                  */
+/************************************************************************/
+void serialEvent1() {
+  while (Serial1.available()) 
+  {
+    // get the new byte:
+    char inChar = (char)Serial1.read();
+    // add it to the inputString:
+    inputString += inChar;
+    
+    //if inputstring is length 6, the full message is received so signal 
+    if (inputString.length() >= 6) 
+    {
+      messageReceived = true;
+    }
+  }
+}
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
+
+/************************************************************************/
+/*                              FUNCTIONS                               */
+/************************************************************************/
+// Function where most of the magic happens, 
+// decifers serial input and sets leds using a color wheel
+void WriteLedStrip()
+{
+  int groupNr = 0; int16_t points = 0;
+  int ledPin = 0; 
+  char grpChar = inputString.charAt(1);
+  String pointsString = inputString.substring(3);
+
+  // Get the group nr and pin nr
+  groupNr = grpChar - '0';
+  ledPin = groupNrToPin(groupNr);
+  // Check if pin is valid otherwise return
+  if(ledPin == 0)
+    return;
+    
+  for(int i = 0; i < pointsString.length(); i++)
+  {
+    if(!isDigit(pointsString.charAt(i)))
+    {
+      // points string not digit, return
+      Serial.println("POINTS NOT ALL DIGITS, NOT SETTING LEDS");
+      return;
+    }
+  }
+
+  // Set points
+  points = pointsString.toInt();
+  if (points > led_strip.numPixels())
+  {
+    points = led_strip.numPixels();
+  }
+  else if (points < 0)
+  {
+    points = 0;
+  }
+
+  Serial.print("Group: ");
+  Serial.print(groupNr);
+  Serial.print("   Points: ");
+  Serial.println(points);
+  
+  // Set ledstrip pin to group ledstrip pin
+  led_strip.clear();
+  led_strip.setPin(ledPin);
+
+  // Get random offset 
+  long randomNr = random(256);
+  uint16_t loopCount = 1;
+  uint16_t delayTime = (5000 / points) - 1;
+  
+  do
+  {
+    for(int16_t i = 0; i < loopCount; i++)
+    {
+      led_strip.setPixelColor(i, Wheel(((i * 256 / led_strip.numPixels()) + randomNr) & 255));
+    }
+    led_strip.show();
+    delay(delayTime);
+    loopCount++;
+  }
+  while(loopCount < points);
+
+  Serial.println("LEDS set! Waiting for new serial command");
+}
+
+int groupNrToPin(int nr)
+{
+  int pin = 0;
+  switch(nr)
+  {
+    case 1: pin = LED_GROUP_1; break;
+    case 2: pin = LED_GROUP_2; break;
+    case 3: pin = LED_GROUP_3; break;
+    case 4: pin = LED_GROUP_4; break;
+    case 5: pin = LED_GROUP_5; break;
+    case 6: pin = LED_GROUP_6; break;
+  }
+
+  return pin;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return led_strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return led_strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
+  }
+  WheelPos -= 170;
+  return led_strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
+}
+/************************************************************************/
+/*                                                                      */
+/************************************************************************/
