5c596886b4c29df8ac0cd86a3c0cdc58cb6e86e1
PepijnHogervorst <pepijnh443@gmail.com>
2025-06-28 09:40:29 +0200

    #9 Add basic DI setup

--- WelpenScoreboard/src/WpfUI/App.xaml.cs
+++ WelpenScoreboard/src/WpfUI/App.xaml.cs
@@ -1,14 +1,39 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
 
-namespace WpfUI
+namespace WelpenScoreboard.WpfUI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private readonly IServiceProvider _serviceProvider;
+
+    public App()
     {
+        ServiceCollection serviceCollection = new();
+        serviceCollection.ConfigureServices();
+
+        _serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions()
+        {
+            ValidateOnBuild = true,
+            ValidateScopes = true,
+        });
     }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        MainWindow window = _serviceProvider.GetRequiredService<MainWindow>();
+        window.Show();
+    }
+
+    private void OnExit(object sender, ExitEventArgs e)
+    {
+        // Dispose of services if needed
+        if (_serviceProvider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
 }
---
+++ WelpenScoreboard/src/WpfUI/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WelpenScoreboard.WpfUI;
+
+public static class DependencyInjection
+{
+    public static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+    }
+}
