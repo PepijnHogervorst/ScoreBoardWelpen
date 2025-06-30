4ac832a1ce31731885c234eeeb314f08bc5bc476
PepijnHogervorst <pepijnh443@gmail.com>
2025-06-30 19:46:46 +0200

    #9 Add material design and basic tab navigation

--- WelpenScoreboard/src/WpfUI/ViewModels/MainViewModel.cs
+++ WelpenScoreboard/src/WpfUI/ViewModels/MainViewModel.cs
@@ -3,10 +3,25 @@
 namespace WelpenScoreboard.WpfUI.ViewModels;
 public class MainViewModel : ViewModelBase
 {
+    private ITabViewModel? _selectedItem;
+
     public MainViewModel(IEnumerable<ITabViewModel> tabs)
     {
         Tabs = [.. tabs.OrderBy(t => t.Order)];
+        SelectedItem = Tabs.FirstOrDefault();
     }
 
     public IEnumerable<ITabViewModel> Tabs { get; }
+    public int SelectedIndex { get; set; }
+    public ITabViewModel? SelectedItem
+    {
+        get => _selectedItem;
+        set
+        {
+            _selectedItem = value;
+            OnPropertyChanged(nameof(SelectedItem));
+        }
+    }
+
+    public string SoftwareVersion => "Not finished";
 }
--- WelpenScoreboard/src/WpfUI/Views/MainWindow.xaml.cs
+++ WelpenScoreboard/src/WpfUI/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
 using WelpenScoreboard.WpfUI.ViewModels;
 
 namespace WelpenScoreboard.WpfUI.Views;
@@ -12,5 +15,22 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = mainViewModel;
+    }
+
+    private void OnSelectedItemChanged(object sender, DependencyPropertyChangedEventArgs e)
+        => MainScrollViewer.ScrollToHome();
+
+    private void DemoItemsListBox_PreviewMouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        //until we had a StaysOpen flag to Drawer, this will help with scroll bars
+        var dependencyObject = Mouse.Captured as DependencyObject;
+
+        while (dependencyObject != null)
+        {
+            if (dependencyObject is ScrollBar) return;
+            dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+        }
+
+        MenuToggleButton.IsChecked = false;
     }
 }
