b3e38cf713024eef2cbdcd5561d377fb2288c402
PepijnHogervorst <pepijnh443@gmail.com>
2025-06-30 19:47:22 +0200

    #9 Import views from existing wpf app
    TODO: change the views to properly work with MVVM

--- WelpenScoreboard/src/WpfUI/Views/PartyView.xaml.cs
+++ WelpenScoreboard/src/WpfUI/Views/PartyView.xaml.cs
@@ -1,28 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
-namespace WelpenScoreboard.WpfUI.Views
+namespace WelpenScoreboard.WpfUI.Views;
+
+/// <summary>
+/// Interaction logic for PartyView.xaml
+/// </summary>
+public partial class PartyView : UserControl
 {
-    /// <summary>
-    /// Interaction logic for PartyView.xaml
-    /// </summary>
-    public partial class PartyView : UserControl
+    public PartyView()
     {
-        public PartyView()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
+
+    private void SliderBrightness_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+    {
+
+    }
+
+    private void SliderSpeed_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+    {
+
+    }
+
+    private void SliderStrobe_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+    {
+
     }
 }
