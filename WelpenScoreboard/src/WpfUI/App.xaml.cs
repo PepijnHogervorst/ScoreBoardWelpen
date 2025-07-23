f0f0c17141d3b39a4063e9ac97baf2784c160ac9
PepijnHogervorst <pepijnh443@gmail.com>
2025-07-23 21:10:46 +0200

    #9 Add appsettings.json configuration + logger

--- WelpenScoreboard/src/WpfUI/App.xaml.cs
+++ WelpenScoreboard/src/WpfUI/App.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System.Windows;
+using WelpenScoreboard.WpfUI.Services;
 using WelpenScoreboard.WpfUI.Views;
 
 namespace WelpenScoreboard.WpfUI;
@@ -7,24 +10,34 @@ namespace WelpenScoreboard.WpfUI;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<App> _logger;
 
     public App()
     {
+        _configuration = BuildConfig();
+
         ServiceCollection serviceCollection = new();
-        serviceCollection.ConfigureServices();
+        serviceCollection.ConfigureServices(_configuration);
 
         _serviceProvider = serviceCollection.BuildServiceProvider(new ServiceProviderOptions()
         {
             ValidateOnBuild = true,
             ValidateScopes = true,
         });
+
+        var loggerFactory = _serviceProvider.GetRequiredService<ILoggerFactory>();
+        _logger = loggerFactory.CreateLogger<App>();
     }
 
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
+        var startup = _serviceProvider.GetRequiredService<Startup>();
+        await startup.InitializeAsync();
+
         var window = _serviceProvider.GetRequiredService<MainWindow>();
         window.Show();
     }
@@ -37,4 +50,11 @@ public partial class App : Application
             disposable.Dispose();
         }
     }
+
+    private static IConfiguration BuildConfig() => new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
 }
--- WelpenScoreboard/src/WpfUI/DependencyInjection.cs
+++ WelpenScoreboard/src/WpfUI/DependencyInjection.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using System.Reflection;
 using WelpenScoreboard.Domain.Common.Extensions;
+using WelpenScoreboard.Infrastructure;
 using WelpenScoreboard.WpfUI.ViewModels.Interfaces;
 
 namespace WelpenScoreboard.WpfUI;
@@ -9,10 +12,32 @@ public static class DependencyInjection
 {
     private readonly static Assembly _wpfAssembly = typeof(DependencyInjection).Assembly;
 
-    public static void ConfigureServices(this IServiceCollection services)
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        AddApplicationLogging(services, configuration);
+
+        services.AddInfrastructure();
+
+        services.AddServices();
         services.AddSingleton<Views.MainWindow>();
         services.AddSingleton<ViewModels.MainViewModel>();
         services.RegisterAssemblyTypes<ITabViewModel>(ServiceLifetime.Singleton, _wpfAssembly);
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddSingleton<Services.Startup>();
+
+        return services;
+    }
+
+    private static IServiceCollection AddApplicationLogging(this IServiceCollection services, IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        services.AddLogging(builder => builder.AddSerilog(dispose: true));
+        return services;
     }
 }
