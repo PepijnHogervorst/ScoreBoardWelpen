a936aded18af6db73436665e7c5ccaff9a90868d
PepijnHogervorst <pepijnh443@gmail.com>
2025-07-23 20:46:16 +0200

    #9 Auto inject always active bg workers

---
+++ WelpenScoreboard/src/Domain/Common/Interfaces/IAlwaysActiveBackgroundWorker.cs
@@ -0,0 +1,9 @@
+﻿namespace WelpenScoreboard.Domain.Common.Interfaces;
+/// <summary>
+/// Exactly same as <see cref="IBackgroundWorker"/> but every implementation
+/// is added to the DI container at startup (using assembly scanning) 
+/// and directly run after app initialization 
+/// </summary>
+public interface IAlwaysActiveBackgroundWorker : IBackgroundWorker
+{
+}
--- WelpenScoreboard/src/Infrastructure/DependencyInjection.cs
+++ WelpenScoreboard/src/Infrastructure/DependencyInjection.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using ScalableHMI.Domain.Common.Interfaces;
+using System.Reflection;
 using WelpenScoreboard.Application.Mqtt;
+using WelpenScoreboard.Domain.Common.Extensions;
 
 namespace WelpenScoreboard.Infrastructure;
 public static class DependencyInjection
 {
+    private static readonly Assembly _infrastructureAssembly = typeof(DependencyInjection).Assembly;
+
     public static void AddInfrastructure(this IServiceCollection services)
+        services.RegisterAssemblyTypes<IAlwaysActiveBackgroundWorker>(ServiceLifetime.Singleton, _infrastructureAssembly);
     {
         services.AddSingleton<IMqttBroker, Mqtt.MqttNetBroker>();
     }
