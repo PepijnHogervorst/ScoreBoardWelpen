240e382bb127de49eea74f0be7a95ef99f742952
PepijnHogervorst <pepijnh443@gmail.com>
2025-07-23 21:11:30 +0200

    #9 Add startup

---
+++ WelpenScoreboard/src/WpfUI/Services/Startup.cs
@@ -0,0 +1,24 @@
+﻿using WelpenScoreboard.Application.Mqtt;
+
+namespace WelpenScoreboard.WpfUI.Services;
+internal class Startup
+{
+    private readonly IMqttControl _mqttControl;
+
+    public Startup(IMqttControl mqttControl)
+    {
+        _mqttControl = mqttControl;
+    }
+
+    public async Task InitializeAsync()
+    {
+        // Start the MQTT broker and client
+        await StartMqtt();
+    }
+
+    private async Task StartMqtt()
+    {
+        if (await _mqttControl.StartAsync()) return;
+        Console.WriteLine("Failed to start MQTT broker or client.");
+    }
+}
