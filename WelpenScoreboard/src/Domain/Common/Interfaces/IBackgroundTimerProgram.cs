9c8fbbb2316355e260903960f8d70ef172ecd126
PepijnHogervorst <pepijnh443@gmail.com>
2025-07-23 20:35:24 +0200

    #0 Add extensions and helper classes

---
+++ WelpenScoreboard/src/Domain/Common/Interfaces/IBackgroundTimerProgram.cs
@@ -0,0 +1,8 @@
+﻿namespace WelpenScoreboard.Domain.Common.Interfaces;
+/// <summary>
+/// Interface for a background program that runs a task non stop
+/// </summary>
+public interface IBackgroundTimerProgram : IBackgroundProgram
+{
+    TimeSpan Interval { get; }
+}
---
+++ WelpenScoreboard/src/Domain/Common/ValueDescription.cs
@@ -0,0 +1,9 @@
+﻿namespace WelpenScoreboard.Domain.Common;
+/// <summary>
+/// Holds enum value and its description.
+/// </summary>
+public class ValueDescription
+{
+    public Enum? Value { get; set; }
+    public string? Description { get; set; }
+}
