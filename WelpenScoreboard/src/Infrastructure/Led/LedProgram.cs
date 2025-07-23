e15610dfa749a98f64f32ce7df194ade0e1c8f7b
PepijnHogervorst <pepijnh443@gmail.com>
2025-07-23 21:11:40 +0200

    #9 Add led program TODO

---
+++ WelpenScoreboard/src/Infrastructure/Led/LedProgram.cs
@@ -0,0 +1,31 @@
+﻿using WelpenScoreboard.Application.Mqtt;
+using WelpenScoreboard.Application.Mqtt.Events;
+
+namespace WelpenScoreboard.Infrastructure.Led;
+internal class LedProgram
+{
+    private readonly IMqttClient _mqttClient;
+
+    public LedProgram(IMqttClient mqttClient)
+    {
+        _mqttClient = mqttClient;
+    }
+
+    public void Start()
+    {
+        //_mqttClient.OnMessageReceived += OnMqttMessageReceived;
+    }
+
+    public void Stop()
+    {
+        //_mqttClient.OnMessageReceived -= OnMqttMessageReceived;
+    }
+
+    private void OnMqttMessageReceived(object? sender, MqttMessageReceivedEventArgs e)
+    {
+        //switch (switch_on)
+        //{
+        //    default:
+        //}
+    }
+}
